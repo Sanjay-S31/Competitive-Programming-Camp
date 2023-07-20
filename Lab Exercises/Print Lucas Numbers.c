3282bca1c3e4280cd3b9389538a62037124d0f47
Sanjay-S31 <127613655+Sanjay-S31@users.noreply.github.com>
2023-07-20 20:41:56 +0530

    Print Lucas Numbers

---
+++ Lab Exercises/Print Lucas Numbers.c
@@ -0,0 +1,105 @@
+// In Lucas series, the next number is the sum of previous two numbers. The series starts with 2 and 1, and then the next numbers are a sum of the previous 2 numbers.
+
+// For example: 2, 1, 3, 4, 7, 11, 18, 29, etc.
+
+// Here the first 2 numbers are 2 and 1
+
+// The next numbers are:
+
+// · 3 (= 1 + 2)
+
+// · 4 (= 3 + 1)
+
+// · 7 (= 4 + 3)
+
+// · 11 (= 7 + 4)
+
+// And so on…
+
+// Write a program the first n numbers of the Lucas series.
+
+// Hint: You already know the first 2 values. How can you calculate the remaining?
+
+// Input Format
+
+// The first line contains T the number of test cases.
+
+// The following T lines contain n, the input for the Fibonacci Series.
+
+// Output Format
+
+// Print the values on a single line, separated by a space character. At the end of the line, print a new line.
+
+// Sample Input / Output
+
+// Input
+
+// 5
+
+// 3
+
+// 4
+
+// 8
+
+// 9
+
+// 7
+
+// Output
+
+// 2 1 3
+
+// 2 1 3 4
+
+// 2 1 3 4 7 11 18 29
+
+// 2 1 3 4 7 11 18 29 47
+
+// 2 1 3 4 7 11 18
+
+// For example:
+
+// Input	Result
+// 5
+// 3
+// 4
+// 8
+// 9
+// 7
+// 2 1 3
+// 2 1 3 4
+// 2 1 3 4 7 11 18 29
+// 2 1 3 4 7 11 18 29 47
+// 2 1 3 4 7 11 18
+
+
+
+#include<stdio.h>
+int main(){
+    int t;
+    scanf("%d",&t);
+    int n;
+    for(int i=0;i<t;i++){
+        scanf("%d",&n);
+        int f=2,s=1,t;
+        if(n==1){
+            printf("%d",f);
+            return 0;
+        }
+        if(n==2){
+            printf("%d %d",f,s);
+            return 0;
+        }
+        printf("%d %d ",f,s);
+        for(int j=3;j<=n;j++){
+            t=f+s;
+            printf("%d ",t);
+            f=s;
+            s=t;
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
